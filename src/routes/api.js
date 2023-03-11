02012a826ee808067f847b3db1771ca6ee4548b5
mohammadtoosi <35932111+mohammadtoosi@users.noreply.github.com>
2023-03-11 12:04:03 +0330

    Update api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,1 @@
-export const auth = {
-    code: "https://app.iranket.com/api-ref/login?mobile=",
-    register: "https://app.iranket.com/api-ref/register",
-    verify: "https://app.iranket.com/api-ref/login",
-    logout: "https://app.iranket.com/api-ref/login",
-};
-
-export const refer_friends = "https://app.iranket.com/api-ref/refer-friends";
-export const get_order_details =
-    "https://app.iranket.com/api-ref/refer-friends?id_customer=";
-export const set_referal_code = "https://app.iranket.com/api-ref/refer-friends";
-export const update_profile = "https://app.iranket.com/api-ref/identity";
+// hide
